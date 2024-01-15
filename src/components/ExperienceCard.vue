df6674cc727cfa3f47669010674ee08e6ba5cfcd
ilhankalac <ilhan.kalac@codeus.me>
2024-01-15 20:48:06 +0100

    Color of the experience card change

--- src/components/ExperienceCard.vue
+++ src/components/ExperienceCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card>
+  <v-card color="#6D8287">
     <v-card-title
       style="height: 60px; background-color: #596a6e"
       class="text-white d-flex justify-space-between"
@@ -7,31 +7,34 @@
       <span class="ma-2">{{ selectedExperience?.title }}</span>
       <div>
         <v-btn
-          style="color: white"
-          color="#596a6e"
-          flat
-          icon="mdi-close"
-          @click="close"
+            style="color: white"
+            color="#596a6e"
+            flat
+            icon="mdi-close"
+            @click="close"
         />
       </div>
     </v-card-title>
-    <v-sheet
-      class="d-flex pa-2"
-      :class="
-        smAndDown ? 'flex-column justify-center align-center' : 'flex-row'
-      "
+    <div
+      class="d-flex pa-3"
+      :class="smAndDown ? 'flex-column justify-center align-center' : 'flex-row'"
     >
-      <div style="max-width: 200px; max-height: 150px; overflow: hidden">
-        <img
-          style="width: 100%; height: 100%; object-fit: contain"
-          :src="selectedExperience?.logoSrc"
-          alt=""
-        />
-      </div>
-      <v-card-text>
-        {{ selectedExperience?.description }}
-      </v-card-text>
-    </v-sheet>
+        <div 
+            class="ml-2" 
+            style="max-width: 200px; 
+            max-height: 150px; 
+            overflow: hidden"
+        >
+            <img
+                style="width: 100%; height: 100%; object-fit: contain"
+                :src="selectedExperience?.logoSrc"
+                alt=""
+            />
+        </div>
+        <v-card-text>
+            {{ selectedExperience?.description }}
+        </v-card-text>
+    </div>
   </v-card>
 </template>
 
