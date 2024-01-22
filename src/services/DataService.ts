1a0ac770d538493bafb70b3beddc98b377499932
ilhankalac <ilhan.kalac@codeus.me>
2024-01-22 19:22:47 +0100

    Functions for reading and pushing values to firebase add

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,9 +1,29 @@
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, onValue, push } from "firebase/database";
 
 const firebaseDatabase = getDatabase();
 
-const write = (path: string, data: any) => {
+const setVal = (path: string, data: any) => {
   set(ref(firebaseDatabase, path), data);
 }
 
-export { write };
+const getVal = (path: string) => { 
+  const nodePath = ref(firebaseDatabase, path);
+  let value = null;
+
+  onValue(nodePath, (snapshot) => {
+    const data = snapshot.val();
+    value = data;
+  });
+
+  return value;
+}
+
+const pushVal = (path: string, data: any) => {
+  const dataListRef = ref(firebaseDatabase, path);
+  const newPostRef = push(dataListRef);
+  set(newPostRef, {
+    ...data
+  });
+}
+
+export { setVal, getVal, pushVal };
