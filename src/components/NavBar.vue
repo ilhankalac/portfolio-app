280332c13d252501eb5fdfa170ed7b6a98e5b124
ilhankalac <ilhan.kalac@codeus.me>
2024-01-27 21:25:52 +0100

    Progress linear add to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,31 @@
 <template>
-    <div
-        class="d-flex justify-end align-center"
-        :class="smAndDown ? 'flex-column' : 'flex-row'"
-        style="height: 10vh; background-color: rgba(0, 0, 0, 0.3); position:sticky; top:0; z-index: 1000;"
+  <v-progress-linear
+    v-if="!isDataLoaded"
+    indeterminate
+    color="secondary"
+    style="z-index: 1100; position:absolute"
+  />
+  <div
+    class="d-flex justify-end align-center"
+    :class="smAndDown ? 'flex-column' : 'flex-row'"
+    style="height: 10vh; background-color: rgba(0, 0, 0, 0.3); position:sticky; top:0; z-index: 1000;"
+  >   
+    <v-btn 
+      :variant="clickedButton === '0' ? 'tonal' : 'text'" 
+      @click="scrollToNextSection"
     >   
-        <v-btn 
-            :variant="clickedButton === '0' ? 'tonal' : 'text'" 
-            @click="scrollToNextSection"
-        >   
-            WORK 
-        </v-btn> 
-        <v-btn 
-            :variant="clickedButton === '1' ? 'tonal' : 'text'" 
-        >
-            PERSONAL INTERESTS
-        </v-btn>
-    </div>
+      WORK 
+    </v-btn> 
+    <v-btn 
+      :variant="clickedButton === '1' ? 'tonal' : 'text'" 
+    >
+      PERSONAL INTERESTS
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { Ref, ref } from 'vue';
+import { onMounted, Ref, ref } from 'vue';
 import { useDisplay } from 'vuetify'
 import { useRoute, useRouter } from 'vue-router'
 
@@ -27,9 +33,15 @@ const { smAndDown } = useDisplay()
 const clickedButton: Ref<string> = ref('')
 const router = useRouter()
 
+const isDataLoaded = ref(false)
+onMounted(() => {
+  setTimeout(() => {
+    isDataLoaded.value = true;
+  }, 1000);
+})
 const changeThePage = (buttonNumber: string, pageName: string) => {
-    clickedButton.value = buttonNumber;
-    router.push({ name: pageName }); 
+  clickedButton.value = buttonNumber;
+  router.push({ name: pageName }); 
 }
 
 const scrollToNextSection = () => {
