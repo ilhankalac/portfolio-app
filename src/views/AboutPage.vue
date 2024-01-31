7a070e46043496d366b3b43b442baad1bc1680d9
ilhankalac <ilhan.kalac@codeus.me>
2024-02-01 00:03:34 +0100

    Avatar in about me section add

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -8,25 +8,37 @@
 	>
     <div 
       class="d-flex ga-10 align-center font-weight-light "
-      :class="smAndDown ? 'flex-column text-center' : 'justify-center text-justify'"
+      :class="smAndDown ? 'flex-column text-center' : ' flex-column justify-center text-justify'"
       :style="smAndDown ? 'max-width: 500px;' : 'max-width: 1000px;'"
     >
-      <div
-        style="white-space: nowrap; min-width: 200px;"
+      <span 
+        class="text-h4 mb-2" 
       >
-        <span 
-          class="text-h4 mb-2" 
+        About me
+      </span>
+      <div 
+        :class="smAndDown ? 'd-flex flex-column ga-5' : 'd-flex align-center ga-10'"
+      >
+        <div
+          style="white-space: nowrap; min-width: 200px;"
         >
-          About me
-        </span>
+        <div class="d-flex flex-column align-center ga-5">
+          <v-avatar size="150" variant="outlined">
+            <v-img
+              src="@/assets/profile_photo.jpg"
+              alt="avatar"
+            />
+          </v-avatar>
+        </div>
+        </div>
+        <v-divider :thickness="2" vertical/>
+        <v-sheet color="secondary">
+          I'm Ilhan Kalač, a 26-year-old software developer currently based in Podgorica. 
+          I pursued my education at a faculty in Belgrade, laying the groundwork for my tech journey. <br><br>
+          Advanced proficiency in <strong>VueJS</strong> and <strong>Typescript</strong>, with a history of building intuitive and responsive web applications.<br> <br>
+          Beyond coding, my interests span philosophy and the world of cinema. Let's weave together technology, creativity, and philosophy to craft exceptional digital experiences.
+        </v-sheet>
       </div>
-      <v-divider :thickness="2" vertical/>
-      <v-sheet color="secondary">
-        I'm Ilhan Kalač, a 26-year-old software developer currently based in Podgorica. 
-        I pursued my education at a faculty in Belgrade, laying the groundwork for my tech journey. <br><br>
-        Advanced proficiency in <strong>VueJS</strong> and <strong>Typescript</strong>, with a history of building intuitive and responsive web applications.<br> <br>
-        Beyond coding, my interests span philosophy and the world of cinema. Let's weave together technology, creativity, and philosophy to craft exceptional digital experiences.
-      </v-sheet>
     </div>
   </v-sheet>
 </template>
