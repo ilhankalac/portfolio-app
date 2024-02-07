92e0cf1c76b5a4749e80b3f7256e290fca720fd6
ilhankalac <ilhan.kalac@codeus.me>
2024-02-08 00:14:11 +0100

    Comment change

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ body {
 	background-color: rgb(var(--v-theme-primary));
 	color: white;
 
-  /* Add smaller font size for mobile devices */
+  /* Smaller font size for mobile devices */
   @media (max-width: 600px) {
     font-size: 0.8rem;
   }
