d1a32085f8f07f047ade2c30f394405992c74462
ilhankalac <ilhan.kalac@codeus.me>
2024-02-08 00:13:29 +0100

    Font size change

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,10 @@
 body {
 	background-color: rgb(var(--v-theme-primary));
 	color: white;
+
+  /* Add smaller font size for mobile devices */
+  @media (max-width: 600px) {
+    font-size: 0.8rem;
+  }
 }
 </style>
