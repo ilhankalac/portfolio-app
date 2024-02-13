22c34313dfc13bb84f259d28773cae6c313f2325
ilhankalac <ilhan.kalac@codeus.me>
2024-02-13 23:51:33 +0100

    Basic info refactor

--- src/components/BasicInfo.vue
+++ src/components/BasicInfo.vue
@@ -1,14 +1,14 @@
 <template>
   <v-card
     class="d-flex align-center justify-space-between"
-    :class="smAndDown ? '' : 'flex-column'"
+    :class="{ 'flex-column': !smAndDown }"
     flat
     color="secondary"
   >
     <v-avatar
       :size="smAndDown ? '120' : '200'"
       color="white"
-      :class="smAndDown ? '' : 'ma-7'"
+      :class="{ 'ma-5': !smAndDown }"
     >
       <v-img
         alt="Avatar"
@@ -22,33 +22,29 @@
       <v-card-subtitle d-subtitle class="d-flex ga-3 font-weight-light">
         <v-icon> mdi-email </v-icon>
         <a class="font-weight-light" href="mailto:ilhandeveloper@gmail.com">
-          ilhandeveloper@gmail.com</a
-        >
+          ilhandeveloper@gmail.com
+        </a>
       </v-card-subtitle>
       <v-card-subtitle class="d-flex ga-3 font-weight-light">
         <v-icon> mdi-phone </v-icon>
         <a href="tel:+38268186196" style="color: white; text-decoration: none">
-          +382 68 186 196</a
-        >
+          +382 68 186 196
+        </a>
       </v-card-subtitle>
       <v-card-subtitle class="d-flex ga-3 font-weight-light">
         <v-icon> mdi-github </v-icon>
         <a href="https://github.com/ilhankalac" target="_blank">@ilhankalac</a>
       </v-card-subtitle>
-
       <v-card-subtitle class="d-flex ga-3 font-weight-light">
         <v-icon> mdi-linkedin </v-icon>
         <a href="https://www.linkedin.com/in/ilhankalac/" target="_blank"
-          >@ilhankalac</a
-        >
+          >@ilhankalac
+        </a>
       </v-card-subtitle>
       <v-card-subtitle class="d-flex ga-3 font-weight-light">
         <v-icon>mdi-instagram</v-icon>
         <a href="https://www.instagram.com/vizija/" target="_blank">@vizija</a>
       </v-card-subtitle>
-      <!-- <v-card-actions>
-        <v-btn variant="tonal" block> Download CV </v-btn>
-      </v-card-actions> -->
     </div>
   </v-card>
 </template>
