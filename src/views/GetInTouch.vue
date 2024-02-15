0a6da7d76910864c5d7861ad772810dad586c9a4
ilhankalac <ilhan.kalac@codeus.me>
2024-02-15 23:20:20 +0100

    Validation for email add

--- src/views/GetInTouch.vue
+++ src/views/GetInTouch.vue
@@ -9,13 +9,17 @@
   >
     <div class="my-8">
       <v-text-field
+        ref="emailInput"
         v-model="email"
+        :error-messages="errorMessage"
         label="Your Email"
         color="white"
         variant="outlined"
         bg-color="secondary"
-      />
-  
+        autofocus 
+        @blur="validateEmail"
+      /> <br>
+
       <v-textarea
         v-model="message"
         label="Your Message"
@@ -33,6 +37,17 @@
       >
         Send
       </v-btn>
+
+      <v-snackbar
+        v-model="snackbar"
+        color="primary"
+        rounded="pill"
+        location="right top"
+        min-width="360"
+        min-height="60"
+      >
+          {{ text }}
+        </v-snackbar>
     </div>
   </Section>
 </template>
@@ -41,22 +56,36 @@
 import Section from "@/components/Section.vue";
 import { useDisplay } from "vuetify";
 import { ref } from "vue";
-const { smAndDown } = useDisplay();
 import { pushVal } from "@/services/DataService"
 
 const email = ref("");
 const message = ref("");
+const snackbar = ref(false);
+const text = ref("Your message has been sent!");
+const errorMessage = ref("");
+const emailInput = ref<HTMLInputElement | null>(null);
+
+const validateEmail = () => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return errorMessage.value = !emailRegex.test(email.value) ? "Please enter a valid email" : "";
+};
 
 const sendEmail = () => {
+
+  if (!!validateEmail()) {
+    emailInput.value?.focus();
+    return;
+  }
+
   const dataObj = {
     email: email.value,
     message: message.value
   };
 
   pushVal("contacted", dataObj);
+  snackbar.value = true;
 
   email.value = "";
   message.value = "";
 };
-
 </script>
