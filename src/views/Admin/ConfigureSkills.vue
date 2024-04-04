fa464690cab0587f45fb675f1573aa0802838bbd
ilhankalac <ilhan.kalac@codeus.me>
2024-04-05 00:31:28 +0200

    Skills add and edit implemented

--- src/views/Admin/ConfigureSkills.vue
+++ src/views/Admin/ConfigureSkills.vue
@@ -2,28 +2,109 @@
   <v-container>
     <v-row>
       <v-col>
-        <h3>Skills configuration</h3>
+        <v-card 
+          class="ma-1 pa-2 mb-3 pl-5" 
+          color="secondary"
+          style="border: 1px solid black; cursor: pointer;"
+          @click="openDialog()"
+        >
+          Add new skill +
+        </v-card>
+        <template v-for="(skill, key) in skills">
+          <v-card 
+            class="ma-1 pa-2 pl-5" 
+            color="secondary"
+            style="border: 1px solid black; cursor: pointer;"
+            @click="openDialog(skill, key)"
+          >
+            <span class="font-weight-light">
+              <v-icon>mdi-{{ skill.icon }}</v-icon> {{ skill.name }}
+            </span>
+          </v-card>
+        </template>
       </v-col>
     </v-row>
   </v-container>
+  <v-dialog 
+    v-model="addEditSkillDialog"
+    max-width="600"
+  >
+    <v-card color="secondary">
+      <v-card-title>
+        Add new skill
+      </v-card-title>
+      <v-card-text>
+        <v-text-field
+          v-model="skill.name"
+          label="Skill name"
+          required
+        />
+        <v-text-field
+          v-model="skill.icon"
+          label="Icon"
+          required
+        />
+      </v-card-text>
+      <v-btn 
+        class="mx-6 mb-5" 
+        variant="outlined" 
+        @click="save(skill)"
+      >
+        Save
+      </v-btn>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
-import { setVal, getVal } from '@/services/DataService'
-const description = ref('');
+import { setVal, getVal, pushVal } from '@/services/DataService'
 
-const save = () => {
-  setVal('about', description.value);
+const skills: any = ref([]);
+const addEditSkillDialog = ref(false);
+const origin = ref('');
+
+const skill = ref({
+  name: '',
+  icon: '',
+});
+
+const openDialog = (selectedItem: any = { name: '', icon: '' }, key: any = '') => {
+  selectedItem.name == '' ? origin.value = 'add' : origin.value = 'edit';
+  selectedItem.key = key;
+  addEditSkillDialog.value = true;
+  skill.value = Object.assign({}, selectedItem);
+}
+
+
+const save = async (skillData: any) => {
+  if (origin.value === 'add') {
+    await pushVal('skills', skillData);
+    skills.value.unshift(skillData);
+    addEditSkillDialog.value = false;
+    return;
+  }
+
+  if (skillData.key !== -1) {
+    skills.value[skillData.key] = skillData;
+    await setVal('skills', skills.value);
+    addEditSkillDialog.value = false;
+    return;
+  }
+}
+
+const getSkills = () => {
+  getVal('skills').then((val) => {
+    if (val) {
+      Object.keys(val).forEach((key) => {
+        skills.value.push({ ...val[key], key });
+      });
+      skills.value = skills.value.reverse()
+    }
+  });
 }
 
 onMounted(() => {
-  getVal('about').then((val) => {
-    if (val) {
-      description.value = val;
-    }
-  });
+  getSkills();
 });
 </script>
--- src/views/landingPage/Skills.vue
+++ src/views/landingPage/Skills.vue
@@ -20,86 +20,19 @@
 <script lang="ts" setup>
 import Section from "@/components/landingPage/Section.vue";
 import { useDisplay } from "vuetify";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import { getVal } from "@/services/DataService";
 
 const { smAndDown } = useDisplay();
-const skills = ref([
-  {
-    id: 1,
-    name: "Vue.js",
-    icon: "vuejs",
-  },
-  {
-    id: 2,
-    name: "TypeScript",
-    icon: "language-typescript",
-  },
-  {
-    id: 3,
-    name: "JavaScript",
-    icon: "language-javascript",
-  },
-  {
-    id: 4,
-    name: "HTML5",
-    icon: "language-html5",
-  },
-  {
-    id: 5,
-    name: "CSS3",
-    icon: "language-css3",
-  },
-  {
-    id: 6,
-    name: "Vuetify",
-    icon: "vuetify",
-  },
-  {
-    id: 7,
-    name: "Firebase",
-    icon: "firebase",
-  },
-  {
-    id: 8,
-    name: "C Sharp",
-    icon: "language-csharp",
-  },
-  {
-    id: 10,
-    name: "SQL",
-    icon: "database",
-  },
-  {
-    id: 11,
-    name: "Git",
-    icon: "git",
-  },
-  {
-    id: 12,
-    name: "Linux/Bash scripting",
-    icon: "linux",
-  },
-  {
-    id: 13,
-    name: "Photoshop",
-    icon: "image",
-  },
-  {
-    id: 14,
-    name: "Video Editing",
-    icon: "video",
-  },
-  {
-    id: 15,
-    name: "UI/UX Design",
-    icon: "palette",
-  },
-  {
-    id: 16,
-    name: "Social Media Marketing",
-    icon: "instagram",
-  },
-]);
+const skills = ref([]);
+
+onMounted(() => {
+  getVal('skills').then((val) => {
+    if (val) {
+      skills.value = val;
+    }
+  });
+});
 </script>
 
 <style lang="scss" scoped>
