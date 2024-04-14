d2ab44040aebfb1321abaedc6a42478633a013bd
ilhankalac <ilhan.kalac@codeus.me>
2024-04-14 15:57:23 +0200

    Semicolon remove from dataservice

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,30 +1,30 @@
-import { getDatabase, ref, set, onValue, push } from "firebase/database";
+import { getDatabase, ref, set, onValue, push } from "firebase/database"
 
-const firebaseDatabase = getDatabase();
+const firebaseDatabase = getDatabase()
 
 const setVal = (path: string, data: any) => {
-  set(ref(firebaseDatabase, path), data);
+  set(ref(firebaseDatabase, path), data)
 }
 
 const getVal = (path: string): Promise<any> => {
   return new Promise((resolve, reject) => {
-    const nodePath = ref(firebaseDatabase, path);
+    const nodePath = ref(firebaseDatabase, path)
 
     onValue(nodePath, (snapshot) => {
-      const data = snapshot.val();
-      resolve(data);
+      const data = snapshot.val()
+      resolve(data)
     }, {
       onlyOnce: true // Ensure the callback is executed only once
-    });
-  });
-};
-
-const pushVal = (path: string, data: any) => {
-  const dataListRef = ref(firebaseDatabase, path);
-  const newPostRef = push(dataListRef);
-  set(newPostRef, {
-    ...data
-  });
+    })
+  })
 }
 
-export { setVal, getVal, pushVal };
+const pushVal = (path: string, data: any) => {
+  const dataListRef = ref(firebaseDatabase, path)
+  const newPostRef = push(dataListRef)
+  set(newPostRef, {
+    ...data
+  })
+}
+
+export { setVal, getVal, pushVal }
