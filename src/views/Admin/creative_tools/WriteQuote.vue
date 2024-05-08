1877dfd0a865cb481322c3d097c1f62dc4858bdc
ilhankalac <ilhan.kalac@codeus.me>
2024-05-08 22:46:00 +0200

    Add and edit quote as dialog

--- src/views/Admin/creative_tools/WriteQuote.vue
+++ src/views/Admin/creative_tools/WriteQuote.vue
@@ -1,15 +1,15 @@
 <template>
   <v-container>
+    <div style="position: absolute; right: 0;" class="mr-6">
+      <v-btn variant="icon" @click="resetQuoteObject">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </div>
     <v-row>
-      <v-col>
+      <v-dialog v-model="configureQuote" max-width="700">
         <v-card style="flex-grow: 1 !important" color="secondary" class="pa-4">
           <v-card-title class="d-flex justify-space-between">
-          	<span>Write Favorite Quote</span>
-            <v-btn 
-              variant="icon"
-              icon="mdi-plus"
-              @click="resetQuoteObject"
-            />
+            <span>Write Favorite Quote</span>
           </v-card-title>
           <v-text-field
             v-model="quote.author"
@@ -17,26 +17,31 @@
             variant="outlined"
           />
 
-					<v-text-field 
-						v-model="quote.imageSrc"
-						label="Image Source"
-						variant="outlined"
-					/>
+          <v-text-field
+            v-model="quote.imageSrc"
+            label="Image Source"
+            variant="outlined"
+          />
 
-          <QuillEditor 
+          <QuillEditor
             v-model:content="quote.text"
-            :key="quote.key"
-            contentType="html" 
+            contentType="html"
             theme="snow"
           />
-          <v-btn color="white" class="mt-5" variant="outlined" @click="save" block>
+          <v-btn
+            color="white"
+            class="mt-5"
+            variant="outlined"
+            @click="save"
+            block
+          >
             Publish
           </v-btn>
         </v-card>
-      </v-col>
+      </v-dialog>
       <v-col>
         <FavoriteQuotes
-          :origin="'admin-panel'" 
+          :origin="'admin-panel'"
           :changedQuote="quote"
           @editQuote="showSelectedQuoteForEdit"
         />
@@ -46,38 +51,43 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
-import { getVal, pushVal, setVal } from '@/services/DataService'
-import FavoriteQuotes from '@/views/Other/FavoriteQuotes.vue'
+import { ref } from "vue";
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import { getVal, pushVal, setVal } from "@/services/DataService";
+import FavoriteQuotes from "@/views/Other/FavoriteQuotes.vue";
 const quote = ref({
-  key: '',
-	author: '',
-	text: '',
-	imageSrc: ''
-})
+  key: "",
+  author: "",
+  text: "",
+  imageSrc: "",
+});
+
+const configureQuote = ref(false);
 
 const save = () => {
-  if(quote.value.key === '') {
-    pushVal('blog/favorite-quotes', quote.value)
+  if (quote.value.key === "") {
+    pushVal("blog/favorite-quotes", quote.value);
   } else {
-    setVal('blog/favorite-quotes/' + quote.value.key, quote.value)
+    setVal("blog/favorite-quotes/" + quote.value.key, quote.value);
   }
-}
+  configureQuote.value = false;
+};
 
 const showSelectedQuoteForEdit = async (val: any) => {
-  await getVal("blog/favorite-quotes/" + val.key).then((selectedQuote) => { 
-    quote.value = {...selectedQuote, key: val.key}
+  await getVal("blog/favorite-quotes/" + val.key).then((selectedQuote) => {
+    quote.value = { ...selectedQuote, key: val.key };
+    configureQuote.value = true;
   });
-}
+};
 
 const resetQuoteObject = () => {
+  configureQuote.value = true;
   quote.value = {
-    key: '',
-    author: '',
-    text: '',
-    imageSrc: ''
-  }
-}
+    key: "",
+    author: "",
+    text: "",
+    imageSrc: "",
+  };
+};
 </script>
