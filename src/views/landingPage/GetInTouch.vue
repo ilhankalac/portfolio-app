7dd9c173eb195e268ce823584be2ddf6c0b8157b
ilhankalac <ilhan.kalac@codeus.me>
2024-07-16 20:15:26 +0200

    Helper composables add and get config component refactor

--- src/views/landingPage/GetInTouch.vue
+++ src/views/landingPage/GetInTouch.vue
@@ -51,36 +51,23 @@
 </template>
 
 <script lang="ts" setup>
-import Section from "@/components/landingPage/Section.vue"
-import { ref } from "vue"
-import { pushVal } from "@/services/DataService"
+import Section from '@/components/landingPage/Section.vue'
+import { Ref, ref } from 'vue'
+import { pushVal } from '@/services/DataService'
+import { useHelpers } from '@/composables/helper'
 
-const email = ref("")
-const message = ref("")
-const snackbar = ref(false)
-const text = ref("Your message has been sent!")
-const errorMessage = ref("")
+const email: Ref<string> = ref('')
+const message: Ref<string> = ref('')
+const snackbar: Ref<boolean> = ref(false)
+const text: Ref<string> = ref('Your message has been sent!')
 const emailInput = ref<HTMLInputElement | null>(null)
 
-const validateEmail = () => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  return errorMessage.value = !emailRegex.test(email.value) ? "Please enter a valid email" : ""
-}
+const { errorMessage, validateEmail, timeStamp } = useHelpers()
 
-const timeStamp = () => {
-  const currentDate = new Date()
-  const day = currentDate.getDate().toString().padStart(2, '0') 
-  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0')
-  const year = currentDate.getFullYear()
-  const hours = currentDate.getHours().toString().padStart(2, '0') 
-  const minutes = currentDate.getMinutes().toString().padStart(2, '0') 
-  const timestamp = `${day}.${month}.${year}. ${hours}:${minutes}`
-  return timestamp
-}
+const validateEmailWrapper = () => validateEmail(email)
 
 const sendEmail = () => {
-
-  if (!!validateEmail()) {
+  if (!!validateEmailWrapper()) {
     emailInput.value?.focus()
     return
   }
@@ -91,10 +78,10 @@ const sendEmail = () => {
     timeStamp: timeStamp()
   }
 
-  pushVal("contacted", dataObj)
+  pushVal('contacted', dataObj)
   snackbar.value = true
 
-  email.value = ""
-  message.value = ""
+  email.value = ''
+  message.value = ''
 }
 </script>
