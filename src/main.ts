b86738dd1568fa4e5c4bcc9b4e85fd14628e7b7a
ilhankalac <ilhan.kalac@codeus.me>
2024-07-21 17:13:53 +0200

    Remove quil from project

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,6 @@
 import { registerPlugins } from '@/plugins'
 import { firebaseApp } from '@/firebase'
 
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
-
 // Components
 import App from './App.vue'
 
@@ -18,7 +15,7 @@ import App from './App.vue'
 import { createApp } from 'vue'
 
 const app = createApp(App)
-app.component('QuillEditor', QuillEditor)
+app.component('QuillEditor')
 registerPlugins(app, firebaseApp)
 
 app.mount('#app')
