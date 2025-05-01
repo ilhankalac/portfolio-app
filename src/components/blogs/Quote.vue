1b9b108b7c4d130f98b0af459e4393c48cad5d07
ilhankalac <ilhan.kalac@codeus.me>
2025-05-01 02:54:31 +0200

    fix: add padding to quote text for improved readability

--- src/components/blogs/Quote.vue
+++ src/components/blogs/Quote.vue
@@ -4,7 +4,7 @@
     :class="origin === 'quote-dialog' && smAndDown ? 'py-10 pr-10' : ''"
     :style="smAndDown ? 'padding-left: 3em' : 'padding:1.2em 30px 1.2em 75px;'"
   >
-    <div style="font-style: italic" class="text-justify" v-html="selectedQuote?.text" />
+    <div style="font-style: italic" class="text-justify pr-7" v-html="selectedQuote?.text" />
     <div class="d-flex align-center justify-space-between align-center pa-0 mt-5 font-weight-regular">
       <span class="font-weight-regular">
         ― &nbsp;{{
